2878e43acad7a470351e0574222a9be33fd2d87f
Fernanda Rosa <ferosa91@gmail.com>
2021-02-15 20:33:43 +0100

    homepage and navbar fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logoName from "../images/logo_text.png";
+import "./Navbar.css";
 
 function Navbar(props) {
   return (
     <div>
       <nav className="navbar bg-light d-flex justify-content-center">
         <Link to={"/"}>
-          <h1 className="text-warning d-flex justify-content-start">
-            <i className="fas fa-home"></i>
-            <h1>Pickr</h1>
-          </h1>
+          <i id="icon" className="fas fa-home fa-1x pe-auto"></i>
+          <img id="img" className="pe-auto" src={logoName} alt="Pickr Logo" />
         </Link>
       </nav>
     </div>
